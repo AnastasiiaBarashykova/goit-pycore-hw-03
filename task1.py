0f6b53fc4661770008da3bdf4f992e0532633321
Anastasiia <barashykova20@gmail.com>
2026-02-09 21:46:11 +0200

    2nd try

--- task1.py
+++ task1.py
@@ -1,7 +1,13 @@
-from datetime import date
+from datetime import datetime
 
-def get_days_from_today(start_date=date(1996, 9, 20)):
-    today = date.today()
-    return (start_date-today).days
+def get_days_from_today(date="1996-09-20"):
+    try:
+        given_date = datetime.strptime(date, "%Y-%m-%d")
+        today = datetime.today()
+
+        return (given_date.date() - today.date()).days
+
+    except ValueError or TypeError or SyntaxError:
+        return "Дата має бути у форматі 'YYYY-MM-DD'"
 
 print(get_days_from_today())
